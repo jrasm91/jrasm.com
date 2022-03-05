796bd0c1b5c9291456d1577fe4ea3c7d341d0ac3
Jason Rasmussen <jason@pravici.com>
2022-03-04 22:09:59 -0700

    fix: update column spacing

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,10 @@
-import { Container } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import * as React from 'react';
 
 export function Card(props: { children?: React.ReactNode }) {
   return (
-    <Container maxW="lg" p="4">
+    <Box maxW="lg" p="4">
       {props.children}
-    </Container>
+    </Box>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ import MeImage from '../me.jpg';
 function Home() {
   return (
     <Layout>
-      <Flex columnGap="3rem" direction={{ base: 'column', lg: 'row' }}>
-        <VStack alignContent="flex-center" mt={{ base: '', lg: '5rem' }} width="full">
+      <Flex gap="3rem" direction={{ base: 'column', lg: 'row' }}>
+        <VStack justify="space-around" alignContent="center" width="full" maxHeight={{ lg: '80vh' }}>
           <Box justify="space-around" align="center">
             <Heading size="2xl" mt="4" mb="8">
               👋 Hi, I'm Jason!
@@ -14,11 +14,13 @@ function Home() {
             <Image m="4" src={MeImage} alt="Jason Rasmussen" borderRadius="50%" />
           </Box>
         </VStack>
-        <VStack width="full">
-          <AboutCard />
-          <SocialCard />
-          <ProjectsCard />
-        </VStack>
+        <Flex mt={{ base: 0, lg: '12' }} direction="column" align={{ base: 'center', lg: 'flex-start' }} width="full">
+          <VStack align="start">
+            <AboutCard />
+            <SocialCard />
+            <ProjectsCard />
+          </VStack>
+        </Flex>
       </Flex>
     </Layout>
   );
