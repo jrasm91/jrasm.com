41761ab07244c74e292e06d54a31fa8a88e5ff7d
Jason Rasmussen <jrasm91@gmail.com>
2024-03-05 10:10:43 -0500

    feat: github link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
-import { Button, Flex, Heading, HStack, Link, useColorModeValue } from '@chakra-ui/react';
+import { Button, Flex, Heading, HStack, IconButton, Link, useColorModeValue } from '@chakra-ui/react';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useColorScheme } from '../hooks';
 import { ThemeButton } from './ThemeButton';
 
@@ -19,6 +21,17 @@ export const Header = () => {
           <Button colorScheme={useColorScheme()} variant="link">
             Blog
           </Button>
+        </Link>
+        <Link mr="2" fontSize="lg" href="https://github.com/jrasm91" isExternal>
+          <IconButton
+            variant="ghost"
+            color={useColorModeValue('black', 'white')}
+            fontSize="1.5em"
+            borderRadius="50%"
+            m="0.25rem"
+            icon={<FontAwesomeIcon icon={faGithub} />}
+            aria-label={'Toggle Theme'}
+          />
         </Link>
         <ThemeButton />
       </HStack>
